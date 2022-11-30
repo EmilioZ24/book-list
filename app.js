43453844ccfc4210ede859e53bcbe22b30b77448
EmilioZ <90624590+EmilioZ24@users.noreply.github.com>
2022-11-30 02:26:01 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const listItems = document.querySelectorAll('#book-list ul li');
+
+Array.from(listItems).forEach(function(item) {
+  item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
+
+// add to  book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value
+  console.log(value)
+});
